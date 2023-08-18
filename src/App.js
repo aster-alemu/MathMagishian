fd04dd72f9986553283a13af144c76dbbd3e7b3e
Aster Alemu <leaster2014w@gmail.com>
2023-08-18 12:32:10 +0300

    Add files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Numbers from './components/Calculator';
+import Quote from './components/Quote';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Numbers />
+      <Quote />
     </div>
   );
 }
